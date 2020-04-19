fbc825cd021895139f80f297773722e17d0297a0
Michael Larionov <6360814+mlarionov@users.noreply.github.com>
2020-04-18 19:50:07 -0500

    Added a wrapper for the regression case

--- category_encoders/pte_utils.py
+++ category_encoders/pte_utils.py
@@ -1,4 +1,4 @@
-from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.base import BaseEstimator, ClassifierMixin, RegressorMixin
 class EncoderWrapper(BaseEstimator, ClassifierMixin):
     def __init__(self, encoder, classifier):
         self.encoder = encoder
@@ -17,3 +17,20 @@ class EncoderWrapper(BaseEstimator, ClassifierMixin):
     
     def predict(self, X):
         return self.predict_proba(X).round()
+
+class EncoderWrapperR(BaseEstimator, RegressorMixin):
+    def __init__(self, encoder, regressor):
+        self.encoder = encoder
+        self.regressor = regressor
+    
+    def fit(self, X, y, **kwargs):
+        self.encoder.fit(X, y)
+        X_transformed = self.encoder.transform(X)
+        y_transformed = self.encoder.expand_y(y)
+        self.regressor.fit(X_transformed, y_transformed)
+    
+    def predict(self, X):
+        X_transformed = self.encoder.transform(X)
+        preds = self.regressor.predict(X_transformed)
+        return self.encoder.average_y(preds)
+    
