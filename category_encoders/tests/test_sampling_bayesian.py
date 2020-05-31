2178453b84ecdb1a9d5a29d46633432a523cad9b
Michael Larionov <6360814+mlarionov@users.noreply.github.com>
2020-05-30 19:46:00 -0500

    Added one more test. Deleted hotdeck encoder due to its poor performance

--- category_encoders/tests/test_sampling_bayesian.py
+++ category_encoders/tests/test_sampling_bayesian.py
@@ -1,10 +1,12 @@
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 from unittest2 import TestCase  # or `from unittest import ...` if on Python 3.4+
 import category_encoders.tests.helpers as th
 import numpy as np
+from category_encoders.pte_utils import *
+from category_encoders.ordinal import OrdinalEncoder
 
 import category_encoders as encoders
-
 
 np_X = th.create_array(n_rows=100)
 np_X_t = th.create_array(n_rows=50, extras=True)
@@ -19,6 +21,7 @@ y_t_reg = pd.DataFrame(np_y_t_reg)
 y = pd.DataFrame(np_y)
 y_t = pd.DataFrame(np_y_t)
 
+
 class TestSamplingBayesianEncoder(TestCase):
 
     def test_classification(self):
@@ -32,3 +35,19 @@ class TestSamplingBayesianEncoder(TestCase):
         enc.fit(X, y_reg)
         th.verify_numeric(enc.transform(X_t))
         th.verify_numeric(enc.transform(X_t, y_t_reg))
+
+    def test_wrapper(self):
+        enc = encoders.PosteriorImputationEncoderBC(verbose=1, n_draws=2,
+                                                    cols=['unique_str', 'invariant', 'underscore', 'none', 'extra', 321,
+                                                          'categorical', 'na_categorical', 'categorical_int'])
+        classifier = RandomForestClassifier(n_estimators=10)
+        wrapper_model = EncoderWrapper(enc, classifier)
+        X_le = OrdinalEncoder().fit_transform(X).fillna(0)
+        inf_values = np.isinf(X_le).sum(axis=1) == 0
+        X_le = X_le[inf_values]
+        y_le = y[inf_values]
+        self.assertFalse(np.any(np.isnan(X_le)))
+        self.assertFalse(np.any(np.isinf(X_le)))
+        wrapper_model.fit(X_le, y_le)
+        preds = wrapper_model.predict(X_le)
+        self.assertEqual(y_le.shape[0], preds.shape[0])
